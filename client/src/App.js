814d1caed5183cdb896ced4b594f72ba23343cfc
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-28 02:30:03 +0530

    Context API- For Account

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import Messenger from './components/Messenger';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import AccountProvider from './context/AccountProvider';
 
 function App() {
   const clientID='';
   return (
     <>
       <GoogleOAuthProvider clientId={clientID}>
+        <AccountProvider>
         <Messenger />
+        </AccountProvider>
       </GoogleOAuthProvider>;
     </>
   );
