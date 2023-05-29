037490839225a643b6f605e1813eea198b8b27c5
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-29 23:55:59 +0530

    HeaderMenu

--- client/src/components/chat/menu/HeaderMenu.jsx
+++ client/src/components/chat/menu/HeaderMenu.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useState, useContext } from 'react';
+
+import { MoreVert } from '@mui/icons-material';
+import { Menu, MenuItem, styled } from '@mui/material';
+
+const MenuOption = styled(MenuItem)`
+    font-size: 14px
+    padding: 15px 60px 5px 24px;
+    color: #4A4A4A;
+`;
 
 const HeaderMenu = () => {
-  return (
-    <div>HeaderMenu</div>
-  )
+
+    const [open, setOpen] = useState(null);
+
+    const handleClick = (event) => {
+        setOpen(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setOpen(null);
+    };
+
+    return (
+        <>
+        <MoreVert onClick={handleClick} />
+            <Menu
+                anchorEl={open}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                getContentAnchorEl={null}
+                anchorOrigin={{ // wrt to anchor (ele which is above it - MoreVert here)
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{ 
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <MenuOption onClick={handleClose}>Profile</MenuOption>
+                <MenuOption onClick={handleClose}>My account</MenuOption>
+                <MenuOption onClick={handleClose}>Logout</MenuOption>
+            </Menu>
+        </>
+    )
 }
 
 export default HeaderMenu
