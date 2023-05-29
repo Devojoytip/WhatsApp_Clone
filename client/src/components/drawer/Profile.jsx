5df7396cdeb828200b01c529ea57a612bc4c13bf
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-30 00:26:45 +0530

    Profile component

---
+++ client/src/components/drawer/Profile.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material"
+
+const Profile = () => {
+  return (
+    <>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+    </>
+  )
+}
+
+export default Profile
