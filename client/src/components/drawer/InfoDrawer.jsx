979616d69dac41f5110c07f50dbc195923b91cb3
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-30 00:24:02 +0530

    Drawer css

--- client/src/components/drawer/InfoDrawer.jsx
+++ client/src/components/drawer/InfoDrawer.jsx
@@ -1,5 +1,33 @@
-import { Drawer } from '@mui/material'
-import React from 'react'
+import { styled, Drawer, Box, Typography } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+
+const Header = styled(Box)`
+  background: #008069;
+  height: 107px;
+  color: #FFFFFF;
+  display: flex;
+  & > svg, & > p {
+    margin-top: auto;
+    padding: 15px;
+    font-weight: 600;
+`;
+
+const Component = styled(Box)`
+  background: #ededed;
+  height: 85%;
+`;
+
+const Text = styled(Typography)`
+    font-size: 18px;
+`
+
+const drawerStyle = {
+    left: 20,
+    top: 17,
+    height: '95%',
+    width: '30%',
+    boxShadow: 'none'
+}
 
 const InfoDrawer = (props) => {
     const {open,setOpen} = props;
@@ -7,8 +35,15 @@ const InfoDrawer = (props) => {
         <Drawer
             open={open}
             onClose={setOpen}
+            PaperProps={{ sx: drawerStyle }}
+            style={{ zIndex: 1500 }}
         >
-            Hello
+            <Header>
+                <ArrowBack onClick={() => setOpen(false)} />
+                <Text>Profile</Text>
+            </Header>
+            <Component>
+            </Component>
         </Drawer>
     )
 }
