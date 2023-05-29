75b3ff516e3f3e3ea9becc92598d14a74b11327a
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-29 23:36:29 +0530

    Header with account info using context api

--- client/src/components/chat/menu/Header.jsx
+++ client/src/components/chat/menu/Header.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useContext, useState } from 'react';
+
+import { Box, styled } from '@mui/material';
+import { Chat as MessageIcon } from '@mui/icons-material';
+
+import { AccountContext } from '../../../context/AccountProvider';
+
+//components
+import HeaderMenu from './HeaderMenu';
+
+const Component = styled(Box)`
+    height: 44px;
+    background: #ededed;
+    display: flex;
+    padding: 8px 16px;
+    align-items: center;
+`;
+
+const Wrapper = styled(Box) `
+    margin-left: auto;
+    & > * {
+        margin-left: 2px;
+        padding: 8px;
+        color: #000;
+    };
+    & :first-child {
+        font-size: 22px;
+        margin-right: 8px;
+        margin-top: 3px;
+    }
+`;
+    
+const Image = styled('img') ({
+    height: 40,
+    width: 40,
+    borderRadius: '50%'
+})
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+
+    const { account } = useContext(AccountContext); // use decoded info to get pic of gmail account
+
+    return (
+        <>
+            <Component>
+                <Image src={account.picture} />
+                <Wrapper>
+                    <MessageIcon />
+                    <HeaderMenu/>
+                </Wrapper>
+            </Component>
+        </>
+    )
 }
 
-export default Header
+export default Header;
