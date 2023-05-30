6386749963a6215e2fa2773bc4ee4e704e057cec
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-31 01:06:06 +0530

    database connected

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-const url=''
+dotenv.config();
+
+const url=process.env.DB_URL
+
 const Connection= async ()=>{
     try {
         await mongoose.connect(url,{useUnifiedTopology:true})
+        console.log('Database connected')
     } catch (error) {
         console.log('Error connecting to database - ',error)
     }
 }
+
+export default Connection
--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import Connection from './database/db.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app=express();
 
-const PORT=8000;
+Connection();
+
+const PORT=process.env.PORT;
 
 app.listen(PORT,()=> console.log(`Server running on port ${PORT}`))
