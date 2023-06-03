f237c8a58272ec1df0d6f01d0a18b506fada075e
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-03 23:24:25 +0530

    getImage controller

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { addUser, getUsers } from '../controllers/user_controller.js';
 import { getConversation, newConversation } from '../controllers/conversation_controller.js';
 import { getMessage, newMessage } from '../controllers/message_controller.js';
-import { uploadImage } from '../controllers/image_controller.js';
+import { uploadImage,getImage } from '../controllers/image_controller.js';
 import storage from '../middlewares/upload.js';
 
 const route = express.Router();
@@ -21,5 +21,6 @@ route.get('/message/get/:id',getMessage)
 
 // File upload routes
 route.post('/file/upload', storage.single('file'), uploadImage);
+route.get('/file/:filename', getImage);
 
 export default route;
